5542822bf0f7a20334dd0694c9bffaf3dda17c53
Taylor Peat <taylorpeat@hotmail.com>
2016-06-02 00:16:16 -0400

    finish inventory

--- inventory/inventory.js
+++ inventory/inventory.js
@@ -1,6 +1,5 @@
 (function() {
   inventory = {
-    retrieveTemplate: function() { return $("#inventory_item").text(); },
     id_num: 1,
     items: [],
     init: function() {
@@ -10,43 +9,50 @@
     },
     setDate: function() {
       var time = new Date();
+
       $("p").text(time.toGMTString());
+    },
+    retrieveTemplate: function() {
+      return $("#inventory_item").text();
     },
     bindEvents: function() {
       $("button").on("click", this.createItem.bind(this));
-      $("#item_table").on("blur", "input", this.saveData.bind(this));
+      $("#item_table").on("blur", "input", this.saveItemInfo.bind(this));
+      $("#item_table").on("click", "a", this.deleteItem.bind(this));
     },
     createItem: function(e) {
       e.preventDefault();
-      var custom_template = this.i_templ.replace(/ID/g, this.id_num);
-      $("#item_table").append(custom_template).on("click", "a", this.deleteItem.bind(this));
+      var item_template = this.i_templ.replace(/ID/g, this.id_num);
+
+      $("#item_table").append(item_template)
       this.saveItem(e.target);
+    },
+    saveItem: function() {
+      this.items.push({ id: this.id_num });
+      this.id_num ++;
+    },
+    saveItemInfo: function(e) {
+      var item = this.findItem(e.target);
+
+      item[e.target.name] = $(e.target).val();
     },
     deleteItem: function(e) {
       e.preventDefault();
-      this.findItem(e.target).
-      $(this).closest("tr").prev().andSelf().remove();
-      this.items = this.items.find(function(item) {
-        return this.findItemId(target) !== item.id;
-      }, this);
-    },
-    saveItem: function() {
-      this.items.push({
-        id: this.id_num
-      });
-      this.id_num ++;
-    },
-    saveData: function(e) {
-      var item = this.findItem(e.target);
-      item[e.target.name] = $(e.target).val();
-    },
-    findItemId: function(item) {
-      return +/\d+/.exec($(item).attr("id"))[0];
+
+      this.findItem(e.target);
+      $(e.target).closest("tr").prev().andSelf().remove();
+      var idx = this.items.indexOf(this.findItem(e.target));
+      if (idx !== undefined) {
+        this.items.splice(idx, 1);
+      }
     },
     findItem: function(target) {
       return this.items.find(function(item) {
         return this.findItemId(target) === item.id;
       }, this);
+    },
+    findItemId: function(target) {
+      return +/\d+/.exec($(target).attr("id"))[0];
     }
   };
 })();
