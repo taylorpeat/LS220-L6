c89cc47dbad4dfd7f1e4ab9109d98246657995a8
Taylor Peat <taylorpeat@hotmail.com>
2016-06-05 00:20:29 -0400

    finish handlebars exercises

--- handlebars_exercises.js
+++ handlebars_exercises.js
@@ -1,10 +1,20 @@
-post = {
+posts = [{
     title: "Sands of Time",
     published_date: "Jan 24, 1967",
-    body: "I've got a nice body!"
-}
+    body: "<strong>I've got a nice body!</strong>",
+    tags: ["great", "inspirational", "truth", "sports"]
+  },
+  {
+    title: "Five for Fighting",
+    published_date: "May 5, 2015",
+    body: "FFIIIIGGGHHT!!!!!!!!!",
+    tags: ["ji"]
+  }];
 
 $(function() {
   var post_template = Handlebars.compile($("#post_template").html());
-  $("body").append(post_template(post));
+  var tag_template = Handlebars.compile($("#tag_template").html());
+  Handlebars.registerPartial("tag_template", $("#tag_template").html());
+
+  $("body").append(post_template({ posts: posts }));
 })
